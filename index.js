57984d94d85d7a12b948acaf09400f4d4fb309ad
ivlen <gh.barreto@hotmail.com>
2021-06-03 00:22:00 -0300

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const keys = require("./config/keys");
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 // connecting to the database
 mongoose.connect(keys.mongoURI, {
@@ -12,10 +12,13 @@ mongoose.connect(keys.mongoURI, {
 // adding app to use express
 const app = express();
 
-require("./routes/storeRouter")(app);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/storeRouter')(app);
 
 // initializing the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Server is running on port:", PORT);
+  console.log('Server is running on port:', PORT);
 });
--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -1,13 +1,15 @@
-const Product = require("../models/Gas");
+const Product = require('../models/Product');
+const util = require('util');
 
 module.exports = app => {
-  app.get("/api/products", async (req, res) => {
+  app.get('/api/products', async (req, res) => {
     const product = await Product.find();
 
     res.send({ product });
   });
 
-  app.get("/api/order", async (req, res) => {
-    console.log("searching");
+  app.post('/api/orders', async (req, res) => {
+    console.log(req.body);
+    res.send('a');
   });
 };
